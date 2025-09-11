2965bd39d9a39c1c72a728f8a5a538bf24b50bb9
kinoppol <kinoppol@hotmail.com>
2025-09-11 07:12:49 +0700

    Update template_header.php

--- template_header.php
+++ template_header.php
@@ -73,7 +73,7 @@
                                                 <li><a href="contact.php">ติดต่อเรา</a></li>
                                                 <!-- Button -->
                                                 <li class="button-header margin-left "><a href="?p=register" class="btn">ลงทะเบียน</a></li>
-                                                <li class="button-header"><a href="../lms/?p=login" class="btn btn3">เข้าสู่ระบบ</a></li>
+                                                <li class="button-header"><a href="../../lms/?p=login" class="btn btn3">เข้าสู่ระบบ</a></li>
                                             </ul>
                                         </nav>
                                     </div>
